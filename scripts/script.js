b2892cb4f511a04d3b8e10c1a61c60c26d084a16
mdabhnx <mdabhnx+github@gmail.com>
2023-02-27 12:11:35 +0600

    slick carousel added with cusotm configs

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+$(".slider-nav").slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true,
+  cssEase: "ease-in-out",
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  // speed: 1000,
+  // fade: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+      },
+    },
+  ],
+});
